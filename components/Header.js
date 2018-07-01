d0119fc1410d15e485316ee946d25f044eff1d71
shadid12 <shadidhaque2012@gmail.com>
2018-06-30 23:00:55 -0400

    added new campaing show form

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
-    <Menu>
-      <Menu.Item>CrowdCoin</Menu.Item>
+    <Menu style={{ marginTop: '10px' }}>
+      <Link route="/">
+        <a className="item">CrowdCoin</a>
+      </Link>
 
       <Menu.Menu position="right">
         <Menu.Item>Campaigns</Menu.Item>
 
-        <Menu.Item>+</Menu.Item>
+        <Link route="/campaigns/new">
+          <a className="item">+</a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
